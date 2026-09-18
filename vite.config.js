a12a66a8eff13bb3c9767fc7f3029be4205d16b2
Sohan Ahammed <sohanahamed884@gmail.com>
2026-09-18 06:47:11 +0600

    refactor: reorganize project files into dedicated directories

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        cert001: resolve(__dirname, 'cert-001.html'),
-        cert002: resolve(__dirname, 'cert-002.html'),
-        cert003: resolve(__dirname, 'cert-003.html'),
-        cert004: resolve(__dirname, 'cert-004.html'),
-        cert006: resolve(__dirname, 'cert-006.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+        cert001: resolve(__dirname, 'pages/cert-001.html'),
+        cert002: resolve(__dirname, 'pages/cert-002.html'),
+        cert003: resolve(__dirname, 'pages/cert-003.html'),
+        cert004: resolve(__dirname, 'pages/cert-004.html'),
+        cert006: resolve(__dirname, 'pages/cert-006.html'),
       },
     },
   },
